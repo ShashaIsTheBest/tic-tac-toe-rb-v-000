fe605fcc423a73f89f34de64f1c6127b4143a368
Shasha Yan <shashayan@gmail.com>
2021-05-16 14:55:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,17 +54,18 @@ def turn(board)
   end
 end
 
-def turn_count(board)
-  turns = 0
-  board.each do {|position| position == "X" || position == "O" }
-  turns += 1
-  end
-  turns
-end
-#
-def current_player(board)
-  turn_count % 2 == 0 ? "X" : "O"
-end
+# def turn_count(board)
+#   turns = 0
+#   board.each do {|position| position == "X" || position == "O" }
+#   turns += 1
+#     end
+#   end 
+#   turns
+# end
+# #
+# def current_player(board)
+#   turn_count % 2 == 0 ? "X" : "O"
+# end
 #
 #
 # def won?(board)
