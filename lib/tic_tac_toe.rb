50884e4eefe50ad426164a349782c671b8832df0
Shasha Yan <shashayan@gmail.com>
2021-05-16 14:48:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,41 +1,9 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-	user_input.to_i-1
-end
-
-def move(board, user_input, current_player = "X")
-  board[user_input.to_i-1] = current_player
-end
-
-def position_taken?(board, user_input)
-  board[user_input] != " "
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board,index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    move(board, index, current_player = "X")
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 WIN_COMBINATIONS = [
@@ -49,14 +17,64 @@ WIN_COMBINATIONS = [
   [6,4,2]
 ]
 
-def won?(board)
-  WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
+def input_to_index(user_input)
+	user_input.to_i-1
+end
+
+# def move(board, index, current_player)
+#   board[index] = current_player
+# end
+#
+def position_taken?(board, input_to_index)
+  if board[input_to_index] != " "
+    true
+  else
+    false
+  end
+end
+#
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board,index)
+    true
+  else
+    false
+  end
+end
+#
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player = "X")
+    display_board(board)
+  else
+    turn(board)
   end
 end
 
+# def turn_count(board)
+#   turns = 0
+#   board.each do |position|
+#   if position == "X" || position == "O"
+#       turns += 1
+#     end
+#   end
+#   turns
+# end
+#
+# def current_player(board)
+#   turn_count(board) % 2 == 0 ? "X" : "O"
+# end
+#
+#
+# def won?(board)
+#   WIN_COMBINATIONS.detect do |combo|
+#     board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0])
+#   end
+# end
+#
 def full?(board)
   board.all?{|token| token == "X" || token == "O"}
 end
@@ -64,16 +82,16 @@ end
 def draw?(board)
   full?(board) && !won?(board)
 end
-
-def over?(board)
-  won?(board) || full?(board) || draw?(board)
-end
-
-def winner(board)
-  if winning_combo = won?(board)
-    board[winning_combo.first]
-  end
-end
+#
+# def over?(board)
+#   won?(board) || full?(board) || draw?(board)
+# end
+#
+# def winner(board)
+#   if winning_combo = won?(board)
+#     board[winning_combo.first]
+#   end
+# end
 
 def play(board)
   while !over?(board)
